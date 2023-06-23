f55c02d2f462917f03930cafa299cc21810de3e0
vishant <vishant.96.sehrawat@gmail.com>
2023-06-23 16:37:51 +0530

    backend major routes added

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,33 @@
 const express = require('express')
-const connection = require('./configs/dbConnection')
-const { questionRouter } = require('./routes/question.route')
-const { scoreRouter } = require('./routes/score.route')
+const {connection} = require('./configs/dbConnection')
+const { questionRouter } = require('./routes/question.routes')
+const { scoreRouter } = require('./routes/score.routes')
+const { userRouter } = require('./routes/user.routes')
+const { createInterviewRouter } = require('./routes/createInterview.routes')
+const { authMiddleware } = require('./middlewares/authMiddleware.middleware')
+const { generateScoreRouter } = require('./routes/generateScore.routes')
 require('dotenv').config()
+
+
 const app = express()
 
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    return res.send("route is working")
+    return res.send({msg:"home route is working"})
 })
 
+app.use("/user",userRouter)
+// ! below auth commented temporarily
+// todo uncomment it 
+// app.user(authMiddleware) 
+app.use("/createInterview",createInterviewRouter)
 app.use("/question", questionRouter)
 app.use("/score", scoreRouter)
-
+app.use("/compare",generateScoreRouter)
 app.listen(process.env.PORT, async () => {
     try {
-        await connection
+        await connection;
         console.log("connected to database");
     } catch (error) {
         console.log(error.message);
